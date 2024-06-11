50e7f1b8c63d105c217f1ccac2099b027ccefdf4
ErronLiu <1347092678@qq.com>
2024-06-11 14:41:27 +0800

    240611-Add ExtractOffsetValues

--- offset/analyse.go
+++ offset/analyse.go
@@ -98,3 +98,53 @@ func AnalyseOffset(path string) error {
 	}
 	return nil
 }
+
+func ExtractOffsetValues(path, dstPath string, col1, col2 int) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	dstFile, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		_ = file.Close()
+		_ = dstFile.Close()
+	}()
+
+	scanner := bufio.NewScanner(file)
+	writer := bufio.NewWriter(dstFile)
+	var sum float64
+	var count int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, "\t")
+		if len(parts[col1]) == 1 || len(parts[col2]) == 1 {
+			continue
+		}
+		f1, err := strconv.ParseFloat(parts[col1], 64)
+		if err != nil {
+			return err
+		}
+		f2, err := strconv.ParseFloat(parts[col2], 64)
+		if err != nil {
+			return err
+		}
+		sub := (f1 - f2) * 2
+		if sub > -10000 && sub < 10000 {
+			count++
+			sum += sub
+		}
+		_, err = writer.WriteString(fmt.Sprintf("%.3f\n", sub))
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("%.6f\n", sum/float64(count))
+
+	return writer.Flush()
+}
